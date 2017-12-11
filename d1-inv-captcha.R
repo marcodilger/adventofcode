1212b4f585245d57c9f4515de6bff7fe01cdd1e9
Marco D <marcodilger@gmx.de>
2017-12-11 10:46:13 +0100

    solution day1 part2

--- d1-inv-captcha.R
+++ d1-inv-captcha.R
@@ -2,9 +2,11 @@
 #    - subtracting string[x]-string[x+1]
 #    - add positions where diff is 0
 
+# part 2: shift second list by half length of original list
+
 
 Captcha <- as.numeric(strsplit(readLines("data/d1-inv-captcha.txt"), "")[[1]])
-CaptchaFollower <- c(Captcha[-1],Captcha[1])
+CaptchaFollower <- c(tail(Captcha, length(Captcha)/2), head(Captcha, length(Captcha)/2))
 CaptchaDiff <- Captcha - CaptchaFollower
 
 Result <- sum(CaptchaFollower[CaptchaDiff==0])
