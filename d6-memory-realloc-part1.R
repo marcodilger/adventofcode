9f2ffa7a57cdfb5a09285f1bfb8e8943cfa6793b
Marco D <marcodilger@gmx.de>
2017-12-13 22:10:51 +0100

    day6 partial solution, too slow

---
+++ d6-memory-realloc-part1.R
@@ -0,0 +1,39 @@
+# --- Day 6: Memory Reallocation ---
+
+# read in data as vector,
+# loop while current state is not in list: append current state to list
+# identify minimum, set minimum to 0, distribute over Vector, save state
+# entries in list is the number of redistribution cycles
+
+banks <- unname(unlist(read.delim("data/d6-memory-realloc.txt", header = FALSE, sep = "\t")))
+banksLength <- length(banks)
+states <- list(banks)
+
+
+repeat {
+
+  pos <- which.max(banks)
+  distrValue <- banks[pos]
+  banks[pos] <- 0
+  while (distrValue > 0)
+  {
+    if (pos == banksLength) {
+      pos <- 1
+    } else {
+      pos <- pos + 1
+    }
+    banks[pos] <- banks[pos] + 1
+    distrValue <- distrValue - 1
+    #print(banks)
+  }
+
+
+  if (list(banks) %in% states) {
+    break
+  } else {
+    states <- c(states, list(banks)) #fehler
+  }
+}
+
+print(length(states))
+
